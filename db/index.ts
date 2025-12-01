796c3ebed2f3c0458a14056ae6f640a8911959dc
widaata <151973729+widaata@users.noreply.github.com>
2025-11-30 19:05:00 -0500

    Enforced type checking for TURSO_DATABASE_URL

--- db/index.ts
+++ db/index.ts
@@ -1,9 +1,13 @@
 import { createClient } from '@libsql/client';
 import { drizzle } from 'drizzle-orm/libsql';
 import * as schema from './schema';
+const dburl = process.env.TURSO_DATABASE_URL;
+if (dburl == undefined) {
+  throw new Error("TURSO_DATABASE_URL is not defined.");
+}
 
 const client = createClient({
-  url: process.env.TURSO_DATABASE_URL, //|| 'file:.data/dev.db',
+  url: dburl, //|| 'file:.data/dev.db',
   authToken: process.env.TURSO_AUTH_TOKEN
 });
 
