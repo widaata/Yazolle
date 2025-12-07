e8cdde9b405470928ea7f0fe5a4563809927053b
widaata <151973729+widaata@users.noreply.github.com>
2025-12-07 00:51:55 -0500

    imported dotenv/config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,19 +1,20 @@
+import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
-const dburl = process.env.TURSO_DATABASE_URL;
-const dbtoken = process.env.TURSO_AUTH_TOKEN;
+//const dburl = process.env.TURSO_DATABASE_URL;
+//const dbtoken = process.env.TURSO_AUTH_TOKEN;
 
-if (dburl == undefined) {
-  throw new Error("TURSO_DATABASE_URL is not defined.")
-}
-if (dbtoken == undefined) {
-  throw new Error("TURSO_AUTH_TOKEN is not defined.")
-}
+//if (dburl == undefined) {
+//  throw new Error("TURSO_DATABASE_URL is not defined.")
+//}
+//if (dbtoken == undefined) {
+//  throw new Error("TURSO_AUTH_TOKEN is not defined.")
+//}
 export default defineConfig({
   schema: './db/schema.ts',
   out: './drizzle',
   dialect: 'turso',
   dbCredentials: {
-    url: dburl,
-    authToken: dbtoken
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN
   }
 });
