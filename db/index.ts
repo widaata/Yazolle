7541b89942f1eacefe41326710237e92df1c833c
widaata <151973729+widaata@users.noreply.github.com>
2025-12-18 00:09:52 -0500

    Removed schema from database connection (index.ts)

--- db/index.ts
+++ db/index.ts
@@ -1,14 +1,14 @@
 import { createClient } from '@libsql/client';
 import { drizzle } from 'drizzle-orm/libsql';
-import * as schema from './schema';
-const dburl = process.env.TURSO_DATABASE_URL;
+//import * as schema from './schema';
+//const dburl = process.env.TURSO_DATABASE_URL;
 
-if (dburl == undefined) {
-  throw new Error("TURSO_DATABASE_URL is not defined.")
-}
+//if (dburl == undefined) {
+// throw new Error("TURSO_DATABASE_URL is not defined.")
+//}
 const client = createClient({
-  url: dburl, //|| 'file:.data/dev.db',
+  url: process.env.TURSO_DATABASE_URL!,//dburl, //|| 'file:.data/dev.db',
   authToken: process.env.TURSO_AUTH_TOKEN
 });
 
-export const db = drizzle(client, { schema });
+export const db = drizzle({ client });
